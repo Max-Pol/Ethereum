1d11c0851cd09a507acbe200d52f6bd13215eacd
Max-Pol Le Brun <maxpol@rythm.co>
2017-01-06 13:46:16 +0100

    generate_charts: format x axis

--- date_app/dateFormat.py
+++ date_app/dateFormat.py
@@ -2,7 +2,12 @@ import datetime as dt
 date_format = "%Y-%m-%d"
 
 
-def tsp_to_date(timestamp):
+def tsp_to_datetime(timestamp):
+    date = dt.datetime.fromtimestamp(timestamp)
+    return date
+
+
+def tsp_to_date_str(timestamp):
     date = dt.datetime.fromtimestamp(timestamp).strftime(date_format)
     return date
 
--- generate_charts.py
+++ generate_charts.py
@@ -2,22 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 import date_app.dateFormat as df
+import matplotlib.dates as mdates
 
 dates = []
 values = []
+
+months = mdates.MonthLocator()
+fig, ax = plt.subplots()
 
 with open('data/data.json', 'r') as f:
     data = json.load(f)
 
 for daily_data in data:
-    print("{}: {}".format(df.tsp_to_date(daily_data['time']),
+    print("{}: {}".format(df.tsp_to_date_str(daily_data['time']),
           daily_data['close']))
-    dates.append(daily_data['time'])
+    # dates.append(df.tsp_to_date(daily_data['time']))
+    dates.append(df.tsp_to_datetime(daily_data['time']))
     values.append(daily_data['close'])
 
 
-x = np.array(dates)
+t = np.array(dates)
 y = np.array(values)
-plt.plot(x, y)
+plt.plot(t, y)
 
 plt.show()
