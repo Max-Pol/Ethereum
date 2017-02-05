d0d932c802521a10558c7b7bc04063513c43ed31
Max-Pol Le Brun <maxpol@rythm.co>
2017-02-05 02:13:31 +0100

    import_data: clean zeros at the beginning

--- import_data.py
+++ import_data.py
@@ -1,36 +1,39 @@
 import requests
 import json
 import date_app.dateFormat as df
-import os
+# import os
 
 
-def get_data(fsym, tsym, time_range):  # TODO: params paremeter in request.get
-    if (time_range == "day"):
-        url = 'https://min-api.cryptocompare.com/data/histoday?' \
-              '&e=CCCAGG&useBTC=true&aggregate=1' \
-              '&fsym=' + fsym +\
-              '&tsym=' + tsym +\
-              '&limit=' + str(df.time_from_creation(fsym, "days"))
-    elif (time_range == "hour"):
-        url = 'https://min-api.cryptocompare.com/data/histohour?' \
-              '&e=CCCAGG&useBTC=true&aggregate=1' \
-              '&fsym=' + fsym +\
-              '&tsym=' + tsym +\
-              '&limit=' + str(df.time_from_creation(fsym, "hours"))  # 2000 MAX
-
-    response = requests.get(url=url)
+def get_data(fsym, tsym, time_range="hours"):
+    url = 'https://min-api.cryptocompare.com/data/histo' + time_range[:-1]
+    params = {"e": "CCCAGG",
+              "useBTC": "true",
+              "aggregate": "1",
+              "fsym": fsym,
+              "tsym": tsym,
+              "limit": str(df.time_from_creation(fsym, time_range))}
+    response = requests.get(url=url, params=params)  # limit params is 2000 max
+    print("Imported from {}".format(url))
     return response.json()
 
 
-data = get_data("ETH", "USD", "day")
-with open('data/data.json', 'w') as f:
+# request data
+data = get_data("ETH", "USD", "days")
+if data["Response"] != "Success":
+    raise ValueError('HTTP Response Error')
+
+# delete zeros at the beginning & first 10 samples
+while data['Data'][0]['volumeto'] == 0:
+    del data['Data'][0]
+for i in range(10):
+    del data['Data'][0]
+
+# save data
+with open('local_data/data.json', 'w') as f:
     json.dump(data["Data"], f)
 
 
-# # Print samples
-# if data["Response"] == "Success":
-#     for daily_data in data["Data"]:
-#         date = df.tsp_to_date(daily_data['time'])
-#         print ('{}: {}'.format(date, daily_data['close']))
-# else:
-#     print ("Error when importing data from the url: ", url)
+# Print data sample
+# for daily_data in data["Data"]:
+#     date = df.tsp_to_date_str(daily_data['time'])
+#     print ('{}: {}'.format(date, daily_data['close']))
